55c9814e446203912d6cad3b5157b9f8e6b9225f
Илюха-денс <cheglakova666@gmail.com>
2025-05-19 14:47:54 +0500

    .

--- includes/parts/header.php
+++ includes/parts/header.php
@@ -3,20 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/EP-08/includes/styles/style.css">
+    <link rel="stylesheet" href="/UP-08-Puchnin-Kriulin/includes/styles/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>УП-08</title>
 </head>
 <body>
     <header>
         <div class="left_header">
-            <img class="logo_img" src="/EP-08/includes/images/logo.png" alt="лого">
-            <a href="/EP-08/index.php" class="h1">Система управления учебным процессом</a>
+            <img class="logo_img" src="/UP-08-Puchnin-Kriulin/includes/images/logo.png" alt="лого">
+            <a href="/UP-08-Puchnin-Kriulin/index.php" class="h1">Система управления учебным процессом</a>
         </div>
         <nav class="right_header">
             <a class="h2" href="/EP-08/pages/students.php">Студенты</a>
-            <a class="h2" href="/EP-08/pages/groups.php">Группы</a>
-            <a class="h2" href="/EP-08/pages/consultation.php">Консультации</a>
+            <a class="h2" href="//pages/groups.php">Группы</a>
+            <a class="h2" href="/UP-08-Puchnin-Kriulin/pages/consultation.php">Консультации</a>
             <a class="h2" href="#">Выйти</a>
         </nav>
     </header>
