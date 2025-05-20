f14713f00d594584cf4eec51704e9d515f4635d2
rxeltyi <kpuchnin38@gmail.com>
2025-05-20 23:28:14 +0500

    немнога изменил

--- includes/parts/footer.php
+++ includes/parts/footer.php
@@ -1,5 +1,6 @@
     <footer>
         <p>© Пучнин - Криулин. Все права защищены. </p>
     </footer>
+    <script src="/UP-08-Puchnin-Kriulin/js/app.js"></script>
 </body>
 </html>
--- includes/parts/header.php
+++ includes/parts/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/UP-08-Puchnin-Kriulin/includes/styles/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="/UP-08-Puchnin-Kriulin/js/jquery-3.7.1.min.js"></script>
     <title>УП-08</title>
 </head>
 <body>
